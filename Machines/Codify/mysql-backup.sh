b45772fe01b347f66a6f76a18ca65b941bb45451
Manuel <manuel.ziwu23@gmail.com>
2024-01-13 03:25:00 +0000

    Pwned codify

---
+++ Machines/Codify/mysql-backup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -x
+DB_USER="root"
+DB_PASS=$(cat notes.txt)
+BACKUP_DIR="/var/backups/mysql"
+
+read -s -p "Enter MySQL password for $DB_USER: " USER_PASS
+echo
+
+if [[ $DB_PASS == $USER_PASS ]]; then
+        echo "Password confirmed!"
+else
+        echo "$USER_PASS"
+        echo "Password confirmation failed!"
+        exit 1
+fi
+
+/usr/bin/mkdir -p "$BACKUP_DIR"
+
+databases=$(/usr/bin/mysql -u "$DB_USER" -h 0.0.0.0 -P 3306 -p"$DB_PASS" -e "SHOW DATABASES;" | /usr/bin/grep -Ev "(Database|information_schema|performance_schema)")
+
+for db in $databases; do
+    /usr/bin/echo "Backing up database: $db"
+    /usr/bin/mysqldump --force -u "$DB_USER" -h 0.0.0.0 -P 3306 -p"$DB_PASS" "$db" | /usr/bin/gzip > "$BACKUP_DIR/$db.sql.gz"
+done
+
+/usr/bin/echo "All databases backed up successfully!"
+/usr/bin/echo "Changing the permissions"
+/usr/bin/chown root:sys-adm "$BACKUP_DIR"
+/usr/bin/chmod 774 -R "$BACKUP_DIR"
+/usr/bin/echo 'Done!'
